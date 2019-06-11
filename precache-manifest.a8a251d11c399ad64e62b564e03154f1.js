3b8e7f3682b31153ccc1b1b3c3e714c51d3b7604
Júlio César Lima <juliocesarlima19@gmail.com>
2019-06-11 00:31:51 -0300

    Updates

---
+++ precache-manifest.a8a251d11c399ad64e62b564e03154f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736de87e8dfe18eb38fd6e91ef0c952d",
+    "url": "/julio-react/index.html"
+  },
+  {
+    "revision": "2ae1b4e23f8421e0d6da",
+    "url": "/julio-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/julio-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2ae1b4e23f8421e0d6da",
+    "url": "/julio-react/static/js/main.51ba54d8.chunk.js"
+  },
+  {
+    "revision": "63567330476a893f31b9",
+    "url": "/julio-react/static/js/runtime~main.2648a2f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/julio-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
