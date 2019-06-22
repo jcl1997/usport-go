1a59e330746dcfb300b9d37320b3b5c14e458913
jcl1997 <juliocesarlima19@gmail.com>
2019-06-22 13:24:28 -0300

    feat(frontend): adiciona validateFormik

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './Login';
+import Login from './js/Login';
 
 ReactDOM.render(
     <Login />,
--- src/js/Login.js
+++ src/js/Login.js
@@ -1,8 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { withFormik } from 'formik';
 import * as rules from './rules';
-import './css/login.css';
-import UserIcon from './img/login/user6.png'
+import './../css/login.css';
+import UserIcon from './../img/login/user6.png'
 
 class Login extends Component {
   render() {
@@ -14,61 +14,81 @@ class Login extends Component {
     } = this.props;
 
     return (
-      <div className="container">
-        <form onSubmit={handleSubmit}>
-          <Fragment>
-            <div className="usuario">
-                <img src={UserIcon} alt="User" />
-            </div>
-            <div  className="login">
-                <h1>Login</h1>
-                <div>
+      <div className="
+        sport
+        d-flex
+        justify-content-center
+        align-items-center">
+        <div className="
+          card form
+          col-10
+          col-md-8
+          col-lg-5
+          col-xl-3
+          text-white">
+          <form
+            className="was-validated"
+            onSubmit={handleSubmit}>
+              <Fragment>
+                <div className="usuario">
+                    <img src={UserIcon} alt="User" />
+                </div>
+                <div  className="login">
+                  <h1>Login</h1>
+                  <div>
                     <p>Usuário</p>
                     <input
                       id="user"
                       name="user"
+                      className="form-control is-invalid"
                       onChange={handleChange}
                       type="email"
                       value={values.user}
                       placeholder="Insira seu nome de usuário" />
-                      <p>{errors.user}</p>
-                </div>
-                <div>
+                      <div className="invalid-feedback">
+                        <p>{errors.user}</p>
+                      </div>
+                  </div>
+                  <div>
                     <p>Senha</p>
                     <input
                       type="password"
                       name="password"
                       id="password"
+                      className="form-control is-invalid"
                       onChange={handleChange}
                       value={values.password}
                       errors={errors.password}
                       placeholder="Insira sua senha" />
-                      <p>{errors.password}</p>
-                </div>
-                <div>
+                      <div className="invalid-feedback">
+                        <p>{errors.password}</p>
+                      </div>
+                  </div>
+                  <div>
                     <input
                       type="submit"
                       name="login"
                       id="login"
                       value="Login"
                       onSubmit={handleSubmit} />
-                </div>
-                <div>
+                  </div>
+                  <div>
                     <ul>
-                        <li><a href="#">Ainda não possui uma conta?</a></li>
-                        <li><a href="#">Esqueceu sua senha?</a></li>
+                      <li><a href="#">Ainda não possui uma conta?</a></li>
+                      <li><a href="#">Esqueceu sua senha?</a></li>
                     </ul>
+                  </div>
                 </div>
-            </div>
-          </Fragment>
-        </form>
-      </div>
+              </Fragment>
+            </form>
+          </div>
+        </div>
     );
   }
 }
 
 export default withFormik({
-  validationOnChange: false,
+  validateOnChange?: false,
   mapPropsToValues: () => ({
     user: 'dev@usportgo.com',
     password: '123456'
