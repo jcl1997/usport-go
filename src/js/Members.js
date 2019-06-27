f9d57a4bfdb748bc587985176dcf3def7eec998f
Julio Cesar Lima <juliocesarlima19@gmail.com>
2019-06-27 18:02:41 -0300

    feat(frontend): style members

--- src/js/Members.js
+++ src/js/Members.js
@@ -9,29 +9,69 @@ export default class Members extends Component {
       firstName,
       secondName,
       nickPlayers,
-      position
+      position,
+      age,
+      city,
+      firstChampion,
+      firstChampionAlt,
+      secondChampion,
+      secondChampionAlt,
+      invert
     } = this.props;
+
+    const className = [
+      'card-body',
+      'row'
+    ];
+
+    if (invert) {
+      className.push('invert');
+    }
+
     return (
-      <div
-        className="card">
-        <div className="row">
-          <div className="col">
-            <img
-              className="
-                rounded-circle
-                d-block
-                mx-auto
-                player"
-              src={player}
-              alt={alt} />
-          </div>
-          <div class="col text-center">
-            <h2 class="titulo">
-              {firstName}
-              <span class="nickjogadores">"{nickPlayers}"</span>
-              {secondName}
-            </h2>
-            <h3>{position}</h3>
+      <div className="pt-2">
+        <div
+          className="card bloco">
+          <div className={className.join(' ')}>
+            <div className="
+              col
+              d-flex
+              justify-content-center
+              align-items-center">
+              <img
+                className="
+                  rounded-circle
+                  d-block
+                  mx-auto
+                  player"
+                src={player}
+                alt={alt} />
+            </div>
+            <div className="col text-center">
+              <h3 className="card-title">
+                {firstName}
+                <span className="nick-player">"{nickPlayers}"</span>
+                {secondName}
+              </h3>
+              <h4>{position}</h4>
+              <p>Idade:&nbsp;{age}</p>
+              <p>Cidade:&nbsp;{city}</p>
+              <p>Campeões Favoritos</p>
+              <div className="
+                row
+                d-flex
+                justify-content-center
+                align-items-center">
+                <img
+                  className="float-left"
+                  src={firstChampion}
+                  alt={firstChampionAlt} />
+                <img
+                  className="float-left"
+                  src={secondChampion}
+                  alt={secondChampionAlt} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/js/Team.js
+++ src/js/Team.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Members from './Members';
-import Robo from './../img/jogadores/Robo.png'
+
+import Robo from './../img/jogadores/Robo.png';
+import Rmble from './../img/campeoes/Rumble.png';
+import Vladmir from './../img/campeoes/Vladimir.png';
+
+import Shrimp from './../img/jogadores/shrimp.png';
+import Karthus from './../img/campeoes/Karthus.png';
+import JarvanIV from './../img/campeoes/JarvanIV.png';
+
+import Goku from './../img/jogadores/goku.png';
+import Zoe from './../img/campeoes/Zoe.png';
+import Ahri from './../img/campeoes/Ahri.png';
 
 export default class Team extends Component {
   render() {
     return (
-      <div className="container-fluid px-lg-5">
+      <div className="container-fluid pt-2 mt-2">
         <Members
           alt="Robo"
           player={Robo}
@@ -13,7 +24,153 @@ export default class Team extends Component {
           secondName="SOUZA"
           nickPlayers="ROBO"
           position="TOPO"
-          city="São Paulo - SP" />
+          age="21"
+          city="São Paulo - SP"
+          firstChampion={Rmble}
+          firstChampionAlt="Rmble"
+          secondChampion={Vladmir}
+          secondChampionAlt="Vladmir"
+          invert={true} />
+        <Members
+          alt="Shrimp"
+          player={Shrimp}
+          firstName="BYEONGHOON"
+          secondName="LEE"
+          nickPlayers="SHRIMP"
+          position="CAÇADOR"
+          age="25"
+          city="Korea - KR"
+          firstChampion={Karthus}
+          firstChampionAlt="Karthus"
+          secondChampion={JarvanIV}
+          secondChampionAlt="Jarvan IV"
+          invert={false} />
+        <Members
+          alt="Goku"
+          player={Goku}
+          firstName="BRUNO"
+          secondName="MIYAGUCHI"
+          nickPlayers="GOKU"
+          position="MEIO"
+          age="22"
+          city="Ilha Solteira - SP"
+          firstChampion={Zoe}
+          firstChampionAlt="Zoe"
+          secondChampion={Ahri}
+          secondChampionAlt="Ahri"
+          invert={true} />
+        <Members
+          alt="Robo"
+          player={Robo}
+          firstName="LEONARDO"
+          secondName="SOUZA"
+          nickPlayers="ROBO"
+          position="TOPO"
+          age="21"
+          city="São Paulo - SP"
+          firstChampion={Rmble}
+          firstChampionAlt="Rmble"
+          secondChampion={Vladmir}
+          secondChampionAlt="Vladmir"
+          invert={true} />
+        <Members
+          alt="Robo"
+          player={Robo}
+          firstName="LEONARDO"
+          secondName="SOUZA"
+          nickPlayers="ROBO"
+          position="TOPO"
+          age="21"
+          city="São Paulo - SP"
+          firstChampion={Rmble}
+          firstChampionAlt="Rmble"
+          secondChampion={Vladmir}
+          secondChampionAlt="Vladmir"
+          invert={true} />
+        <Members
+          alt="Robo"
+          player={Robo}
+          firstName="LEONARDO"
+          secondName="SOUZA"
+          nickPlayers="ROBO"
+          position="TOPO"
+          age="21"
+          city="São Paulo - SP"
+          firstChampion={Rmble}
+          firstChampionAlt="Rmble"
+          secondChampion={Vladmir}
+          secondChampionAlt="Vladmir"
+          invert={true} />
+        <Members
+          alt="Robo"
+          player={Robo}
+          firstName="LEONARDO"
+          secondName="SOUZA"
+          nickPlayers="ROBO"
+          position="TOPO"
+          age="21"
+          city="São Paulo - SP"
+          firstChampion={Rmble}
+          firstChampionAlt="Rmble"
+          secondChampion={Vladmir}
+          secondChampionAlt="Vladmir"
+          invert={true} />
+        <Members
+          alt="Robo"
+          player={Robo}
+          firstName="LEONARDO"
+          secondName="SOUZA"
+          nickPlayers="ROBO"
+          position="TOPO"
+          age="21"
+          city="São Paulo - SP"
+          firstChampion={Rmble}
+          firstChampionAlt="Rmble"
+          secondChampion={Vladmir}
+          secondChampionAlt="Vladmir"
+          invert={true} />
+        <Members
+          alt="Robo"
+          player={Robo}
+          firstName="LEONARDO"
+          secondName="SOUZA"
+          nickPlayers="ROBO"
+          position="TOPO"
+          age="21"
+          city="São Paulo - SP"
+          firstChampion={Rmble}
+          firstChampionAlt="Rmble"
+          secondChampion={Vladmir}
+          secondChampionAlt="Vladmir"
+          invert={true} />
+        <Members
+          alt="Robo"
+          player={Robo}
+          firstName="LEONARDO"
+          secondName="SOUZA"
+          nickPlayers="ROBO"
+          position="TOPO"
+          age="21"
+          city="São Paulo - SP"
+          firstChampion={Rmble}
+          firstChampionAlt="Rmble"
+          secondChampion={Vladmir}
+          secondChampionAlt="Vladmir"
+          invert={true} />
+        <Members
+          alt="Robo"
+          player={Robo}
+          firstName="LEONARDO"
+          secondName="SOUZA"
+          nickPlayers="ROBO"
+          position="TOPO"
+          age="21"
+          city="São Paulo - SP"
+          firstChampion={Rmble}
+          firstChampionAlt="Rmble"
+          secondChampion={Vladmir}
+          secondChampionAlt="Vladmir"
+          invert={true} />
       </div>
     );
   }
